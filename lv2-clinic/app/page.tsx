803b9cbfd436324e2aca00bd1d3afcf00964c2d5
Mwadali <wellingtonmwadali@gmail.com>
2024-07-25 17:52:29 +0300

    project structure

--- lv2-clinic/app/page.tsx
+++ lv2-clinic/app/page.tsx
@@ -1,10 +1,38 @@
 import { Button } from "@/components/ui/button"
+import Image from "next/image";
+import appointmentIcon from '../public/assets/icons/logo-full.svg'
+import PatientForm from "@/components/forms/patientForm"
+import OnboardingImage from "@/public/assets/images/onboarding-img.png"
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-    <h1 className="text-3xl underline  text-white">Home</h1>
-    <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src={appointmentIcon}
+            height={1000}
+            width={1000}
+            alt=" Patient Appointment Logo"
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+        </div>
+        <div className="text-14-regular mt-20 flex justify-between">
+          <p className="justify-items-end text-dark-600 xl:text-left">
+            ©2024 Kidanga Level 4 Hospital
+          </p>
+          <Link href='/?admin=true' className="text-">Admin</Link>
+        </div>
+      </section>
+      <Image
+        src={OnboardingImage}
+        height={1000}
+        width={1000}
+        alt="Onboarding Image"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
---
+++ lv2-clinic/components/forms/patientForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PatientForm = () => {
+  return (
+    <div>patientForm</div>
+  )
+}
+
+export default PatientForm
